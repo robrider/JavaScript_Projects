e85bbf1c8769c18643e10a5d9608a72ee27d78d3
robrider <robertharter@hotmail.com>
2023-11-06 13:55:08 -0800

    += Changes to previous sub

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,15 +1,3 @@
-window.alert("Hello, World!"); /* These 2 statements do the same thing */
-alert("Hello, World");
-var A = "This is a string!"; //Var one
-document.write(A);
-document.write(" Lisa told Bart, \"Knock it off, Or I'll tell dad!\<br>\"Eat my shorts!\"Bart responded." + "<br>");
-document.write("\" Be who you are and say what you feel, "
-+ "because those who mind don\'t matter and those who matter don\'t mind.\""
-+ "-Dr. Seuss" + "<br>");
-
-var B = " Concatenated" + " String" // Var two
-document.write(B + "<br>");
-
 //Var three
 var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", 
     Daughter = "Penny", Son = "Zorro"; 
@@ -27,4 +15,32 @@ var concatenatedSentence = Sent1 + Sent2;
 // Display the concatenated sentence
 console.log(concatenatedSentence);
 
+// Function that uses the += operator
+function concatenateText() {
+    // Concatenating additional text to the existing content
+    var originalText = "Initial text.";
+    var additionalText = " This is additional text.";
+    originalText += additionalText; // Using the += operator
 
+    // Displaying the concatenated text in the HTML element
+    var displayElement = document.getElementById("displayText");
+    displayElement.innerHTML = originalText;
+}
+
+// Call the function when the page loads
+window.onload = concatenateText;
+
+// Function that uses the += operator
+function concatenateText() {
+    // Concatenating additional text to the existing content
+    var originalText = "Initial text.";
+    var additionalText = " This is additional text.";
+    originalText += additionalText; // Using the += operator
+
+    // Displaying the concatenated text in the HTML element
+    var displayElement = document.getElementById("displayText");
+    displayElement.innerHTML = originalText;
+}
+
+// Call the function when the page loads
+window.onload = concatenateText;
---
+++ Basic_JavaScript_Projects/Project2_functions+=/JS/main.js
@@ -0,0 +1,23 @@
+// Function that uses the += operator
+function concatenateText() {
+    // Concatenating additional text to the existing content
+    var originalText = "Initial text.";
+    var additionalText = " This is additional text.";
+    originalText += additionalText; // Using the += operator
+
+    // Displaying the concatenated text in the HTML element
+    var displayElement = document.getElementById("displayText");
+    displayElement.innerHTML = originalText;
+}
+function changeText() {
+    var paragraph = document.getElementById("myParagraph");
+    paragraph.innerHTML = "This paragraph is not the same as the other!";
+}
+
+var additionalText = " This is additional text.";
+var paragraph = document.getElementById("myParagraph");
+paragraph.innerHTML += additionalText;
+
+// Call the function when the page loads
+window.onload = concatenateText;
+
