650f003ab40a0c5b844703565765ea4309e9f4db
robrider <robertharter@hotmail.com>
2023-11-06 00:49:42 -0800

    Basic_JavaScript_Project

---
+++ Basic_JavaScript_Projects/JS/main.js
@@ -0,0 +1,28 @@
+window.alert("Hello, World!"); /* These 2 statements do the same thing */
+alert("Hello, World");
+var A = "This is a string!"; //Var one
+document.write(A);
+document.write(" Lisa told Bart, \"Knock it off, Or I'll tell dad!\<br>\"Eat my shorts!\"Bart responded." + "<br>");
+document.write("\" Be who you are and say what you feel, "
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss" + "<br>");
+
+var B = " Concatenated" + " String" // Var two
+document.write(B + "<br>")
+
+//Var three
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", 
+    Daughter = "Penny", Son = "Zorro"; 
+document.write (Dad + "<br>");
+
+document.write(3 + 3);
+
+// Create two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+//  Alt Concatenate the Sent1 and Sent2 variables
+var concatenatedSentence = Sent1 + Sent2;
+
+// Display the concatenated sentence
+console.log(concatenatedSentence);
