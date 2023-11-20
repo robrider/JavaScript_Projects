def2fba0ded4f1903bd324102460cdb744cfda33
robrider <robertharter@hotmail.com>
2023-11-20 09:38:57 -0800

    Reconstruction BJSP\P6toc

--- Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,17 +1,11 @@
 // Function to check voting eligibility
 function checkVotingEligibility() {
-  // Get user's age from the input
   var userAge = parseInt(prompt("Enter your age:"));
-
-  // Use ternary operator to check eligibility
-  var eligibilityMessage = userAge >= 18 ? "You can vote! Get in that booth!" : "You are not old enough to vote, I have some crayons for you though!";
-
-  // Display the result
+  var eligibilityMessage = userAge >= 18
+    ? "You can vote! Get in that booth!"
+    : "You are not old enough to vote, I have some crayons for you though!";
   alert(eligibilityMessage);
 }
-
-// Call the function when the page loads
-checkVotingEligibility();
 
 // Nested function
 function nestedFunction() {
@@ -20,12 +14,5 @@ function nestedFunction() {
 
 // Function to invoke the nested function
 function invokeNestedFunction() {
-  // Accessing the <p> element by its ID
-  let paragraph = document.getElementById('nestedFunction');
-
-  // Calling the nested function when the <p> element is clicked
-  paragraph.onclick = function () {
-    nestedFunction();
-  };
+  nestedFunction();
 }
-
