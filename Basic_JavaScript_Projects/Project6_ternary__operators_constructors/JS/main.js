ecf7ba4429ded320ef47e8336979cc23b0ffa3cb
robrider <robertharter@hotmail.com>
2023-11-20 14:04:05 -0800

    BJSP_P6 reorganization

--- Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,18 +1,95 @@
 // Function to check voting eligibility
 function checkVotingEligibility() {
-  var userAge = parseInt(prompt("Enter your age:"));
+  var userAge = document.getElementById("userInput").value;
   var eligibilityMessage = userAge >= 18
     ? "You can vote! Get in that booth!"
     : "You are not old enough to vote, I have some crayons for you though!";
-  alert(eligibilityMessage);
+ document.getElementById('vote').innerHTML=eligibilityMessage;
 }
 
-// Nested function
-function nestedFunction() {
-  alert('This is a nested function!');
+function vehicle(make,color){
+  this.vehiclemake=make;
+  this.vehiclecolor=color;
 }
 
-// Function to invoke the nested function
-function invokeNestedFunction() {
-  nestedFunction();
+var robert=new vehicle("jeep","blue");
+
+function myfunction(){
+  document.getElementById('car').innerHTML= "My Car is a " + robert.vehiclecolor + " " + robert.vehiclemake;
 }
+
+    // Nested Function
+function MyNestedFunction() {
+  document.getElementById("nested_function").innerHTML = Count();
+  function Count() {
+      return 5+5;
+  }
+}
+MyNestedFunction();
+
+
+/* Leftovers from PROJECT6_TERNARY__OPERATORS_CONSTRUCTORS ASSIGNMENTS
+// Constructor Function
+    function ExampleConstructor(value) {
+      this.value = value;
+      this.square = function() {
+        return this.value * this.value;
+      };
+    }
+
+    // Function to check voting eligibility
+    function checkVotingEligibility() {
+      var userAge = parseInt(prompt("Enter your age:"));
+      var eligibilityMessage = userAge >= 18
+        ? "You can vote! Get in that booth!"
+        : "You are not old enough to vote, I have some crayons for you though!";
+      alert(eligibilityMessage);
+    }
+
+    // Display Results Function
+    function displayResults() {
+      var inputElement = document.getElementById('userInput');
+      var inputValue = parseInt(inputElement.value);
+
+      // Use the constructor with the 'new' keyword
+      var exampleObject = new ExampleConstructor(inputValue);
+
+      // Display the result in an HTML element
+      var resultDisplay = document.getElementById('resultDisplay');
+      resultDisplay.innerHTML = 'Square of ' + inputValue + ' is: ' + exampleObject.square();
+    }
+
+    // Nested Function
+    function processInput() {
+      // Perform some processing before displaying the result
+      displayResults();
+    }
+F
+// Constructor Function
+function ExampleConstructor(value) {
+  this.value = value;
+  this.square = function() {
+    return this.value * this.value;
+  };
+}
+
+
+
+// Display Results Function
+function displayResults() {
+  var inputElement = document.getElementById('userInput');
+  var inputValue = parseInt(inputElement.value);
+
+  // Use the constructor with the 'new' keyword
+  var exampleObject = new ExampleConstructor(inputValue);
+
+  // Display the result in an HTML element
+  var resultDisplay = document.getElementById('resultDisplay');
+  resultDisplay.innerHTML = 'Square of ' + inputValue + ' is: ' + exampleObject.square();
+}
+
+// Nested Function
+function processInput() {
+  // Perform some processing before displaying the result
+  displayResults();
+} */
