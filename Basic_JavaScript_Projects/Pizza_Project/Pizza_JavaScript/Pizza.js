ceb83fbb6b64b4a0e5335d4761667dd8f00e9bba
robrider <robertharter@hotmail.com>
2023-12-26 16:25:54 -0800

    Update comments on code

--- Basic_JavaScript_Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Basic_JavaScript_Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,9 +1,11 @@
+// Function to generate the receipt when the "Place Order" button is clicked
 function getReceipt() {
     var text1 = "<h3>You Ordered:</h3>";
     var runningTotal = 0;
     var sizeTotal = 0;
     var selectedSize = "";
 
+    // Retrieving the selected pizza size from the radio buttons
     var sizeArray = document.getElementsByClassName("size");
     for (var i = 0; i < sizeArray.length; i++) {
         if (sizeArray[i].checked) {
@@ -12,7 +14,8 @@ function getReceipt() {
         }
     }
 
-    if (selectedSize === "Personal Pizza") {
+     // Calculating the total cost based on the selected pizza size
+         if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
         sizeTotal = 8;
@@ -24,6 +27,7 @@ function getReceipt() {
         sizeTotal = 16;
     }
 
+     // Updating the running total and logging information
     runningTotal = sizeTotal;
     console.log(selectedSize + " = $" + sizeTotal + ".00");
     console.log("size text1: " + text1);
@@ -33,10 +37,13 @@ function getReceipt() {
     getTopping(runningTotal, text1); // Moved the curly brace to the beginning of getTopping
 }
 
+ // Calling the getTopping function with updated values
 function getTopping(runningTotal, text1) {
     var toppingTotal = 0;
     var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
+    
+    // Looping through the checkboxes to get selected toppings
     for (var j = 0; j < toppingArray.length; j++) {
         if (toppingArray[j].checked) {
             selectedTopping.push(toppingArray[j].value);
@@ -50,11 +57,15 @@ function getTopping(runningTotal, text1) {
     } else {
         toppingTotal = 0;
     }
+
+    // Updating the running total with topping cost and logging information
     runningTotal = (runningTotal + toppingTotal);
     console.log("total selected topping items: " + toppingCount);
     console.log(toppingCount + " topping 1 free topping = " + "$" + toppingTotal + ".00");
     console.log("topping text1: " + text1);
     console.log("Purchase Total: " + "$" + runningTotal + ".00");
+    
+    // Updating the HTML to display the receipt
     document.getElementById("showText").innerHTML = text1;
     document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$" +
         runningTotal + ".00" + "</strong></h3>";
